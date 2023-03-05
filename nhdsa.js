ad4fd62aa333419cd46053b4b286859d2462eb66
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-03-05 19:21:30 +0500

    Add files via upload

---
+++ nhdsa.js
@@ -0,0 +1,13 @@
+
+ let age= +prompt("Введите возраст")
+if(age<=18){
+console.log("Вы еще молоды, УЧИСЬ");
+}else if(age<=50){
+    console.log("Вам нужно работать");
+}else if(age<=59){
+    console.log("Скоро на пенсию");
+}else if(age<=100){
+    console.log("Вы пенсионер");
+}else{
+    console.log("Who are you??");
+}
